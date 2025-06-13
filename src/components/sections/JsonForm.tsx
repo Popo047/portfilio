95c7d54d57cd1ed7e52c54e8438c44095dd03be7
Popo <sanchay2234@gmail.com>
2025-06-14 03:52:42 +0530

    Added a TextArea for mobile

--- src/components/sections/JsonForm.tsx
+++ src/components/sections/JsonForm.tsx
@@ -7,6 +7,7 @@ import { toast } from "sonner";
 import CodeMirror from "@uiw/react-codemirror";
 import { json } from "@codemirror/lang-json";
 import { SectionData } from "@/app/admin/types";
+import { Textarea } from "../ui/textarea";
 
 type JsonFormProps = {
 	onSave: (data: SectionData) => void;
@@ -43,18 +44,45 @@ export function JsonForm({ onSave }: JsonFormProps) {
 	};
 
 	return (
-		<div className="p-4 max-w-4xl mx-auto space-y-4">
-			<h2 className="text-xl font-semibold">Paste your JSON config</h2>
-			<CodeMirror
-				value={jsonInput}
-				height="400px"
-				extensions={[json()]}
-				onChange={(value) => setJsonInput(value)}
-				className="border rounded-md"
-				theme="dark"
-			/>
-			{error && <p className="text-red-500 text-sm">{error}</p>}
-			<Button onClick={handleSubmit}>Save</Button>
+		<div>
+			<div className="md:flex  md:flex-col hidden p-4 max-w-4xl mx-auto space-y-4">
+				<CodeMirror
+					value={jsonInput}
+					height="400px"
+					extensions={[json()]}
+					onChange={(value) => setJsonInput(value)}
+					className="border rounded-md"
+					theme="dark"
+				/>
+
+				{error && <p className="text-red-500 text-sm">{error}</p>}
+				<Button onClick={handleSubmit}>Save</Button>
+			</div>
+			<div className="md:hidden flex flex-col p-4 max-w-4xl mx-auto space-y-4">
+				<Textarea
+					rows={20}
+					value={jsonInput}
+					onChange={(e) => setJsonInput(e.target.value)}
+					placeholder={`{
+	"about": {
+		"name": "Soham Debnath",
+		"content": "I'm a passionate developer..."
+	},
+	"skills": {
+		"skillGroups": [
+			{
+				"category": "Frontend",
+				"skills": ["React", "Next.js", "TailwindCSS"]
+			}
+		]
+	}
+}`}
+					className="font-mono text-sm"
+				/>
+				{error && <p className="text-red-500 text-sm">{error}</p>}
+
+				<Button onClick={handleSubmit}>Save</Button>
+			</div>
 		</div>
 	);
 }
