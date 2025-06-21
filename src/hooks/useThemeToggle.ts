d9552e8035f7cc30b659b809ab6f6ee79f89f5e9
Popo <sanchay2234@gmail.com>
2025-06-21 18:20:07 +0530

    default to dark

--- src/hooks/useThemeToggle.ts
+++ src/hooks/useThemeToggle.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 export type Theme = "light" | "dark";
 
 export function useThemeToggle() {
-	const [theme, setTheme] = useState<Theme>("light");
+	const [theme, setTheme] = useState<Theme>("dark");
 
 	useEffect(() => {
 		const storedTheme = localStorage.getItem("theme") as Theme;
