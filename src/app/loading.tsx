33af1565f6e5c3a5db32bdbc2e93b99371797fbd
Soham Debnath <sanchay2234@gmail.com>
2025-07-22 19:51:40 +0530

    Added loading.tsx ar root level for useSearchParams in About

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+function loading() {
+	return (
+		<div className="h-screen justify-center items-center">Loading ...</div>
+	);
+}
+
+export default loading;
